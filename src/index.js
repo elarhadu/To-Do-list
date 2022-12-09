0520bf4c3abfc21373bf716d9fd483e1011176a5
elarhadu <elarhadu@gmail.com>
2022-12-09 22:02:48 +0000

    Add edit, remove and delete functionalities

--- src/index.js
+++ src/index.js
@@ -1,31 +1,79 @@
-import 'lodash';
-import './style.css';
-import tasksLists from './modules/render.js';
+import "lodash";
+import "./style.css";
 
-class Display {
-  static showTasks() {
-    const lists = tasksLists;
+const inputField = document.querySelector(".add-form input");
+let taskLists = [];
+const todoLists = document.querySelector(".tasks-list");
+let editIndex;
+let editedTodo = false;
 
-    const tasks = lists;
 
-    tasks.forEach((task) => Display.addTaskToList(task));
-  }
+const tasksTodo = (todo) => {
+  const displayTasks = document.createElement("div");
 
-  static addTaskToList(task) {
-    const todoLists = document.querySelector('.tasks-list');
-    const displayTasks = document.createElement('div');
-    displayTasks.innerHTML = `
-   <div class = "todo-list" id = ${task.index} complete = ${task.completed}>
-   <div class = "checkbox-description-container">
-        <input type='checkbox'>
-        <p>${task.description}</p>
-   </div>
-     <i class="fa-solid fa-ellipsis-vertical"></i>
+  displayTasks.innerHTML += `
+     <div class = "todo-list" id = ${todo.index} complete= ${todo.completed}>
+     <div class = "checkbox-description-container">
+          <input type='checkbox' onclick='completedTask(${todo.completed})' >
+          <input type='text' value=${todo.description} class="user-task" readonly>
      </div>
-    <div class="line"></div>
-     `;
-    todoLists.appendChild(displayTasks);
+     <div class= "edit-remove" >
+       <i class="fa-solid fa-ellipsis-vertical" ></i>
+       <ul>
+       <li class="edit" onclick="editTask(${todo.index}, ${todo.description})"><span class="material-symbols-outlined">
+       edit
+       </span>Edit</li>
+       <li onclick="removeTask(${todo.index})"><span class="material-symbols-outlined">
+       delete
+       </span>Remove</li>
+       </ul>
+       </div>
+       </div>
+      <div class="line"></div>
+       `;
+
+  todoLists.appendChild(displayTasks);
+};
+
+inputField.addEventListener("keypress", (e) => {
+  let todoLists = inputField.value;
+  if (e.key === "Enter" && todoLists) {
+    if (localStorage.getItem("duties") === null) {
+      taskLists = [];
+    } else {
+      taskLists = JSON.parse(localStorage.getItem("duties"));
+    }
+    let assignment = {
+      description: todoLists,
+      completed: false,
+      index: taskLists.length,
+    };
+    e.target.value = "";
+    taskLists.push(assignment);
+    localStorage.setItem("duties", JSON.stringify(taskLists));
+    tasksTodo(assignment);
   }
+});
+
+window.removeTask = (index) => {
+  const taskId = document.getElementById(index);
+  const deleteTask = JSON.parse(localStorage.getItem("duties"));
+  const newTodo = deleteTask.filter((task) => {
+    return task.index !== index;
+  });
+  localStorage.setItem("duties", JSON.stringify(newTodo));
+  taskId.remove();
+};
+
+const tasks = JSON.parse(localStorage.getItem("duties")) || [];
+if (localStorage.getItem("duties")) {
+  tasks.forEach((task) => {
+    tasksTodo(task);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', Display.showTasks);
+// Edit a task
+window.editTask = (index, description) => {
+  editIndex=index;
+  inputField.value = description;
+}
--- src/modules/render.js
+++ src/modules/render.js
@@ -1,14 +1,24 @@
-const tasksLists = [
-  {
-    index: 1,
-    description: 'Wash the dishes',
-    completed: false,
-  },
-  {
-    index: 2,
-    description: 'Complete To Do List project',
-    completed: false,
-  },
-];
+// const tasksLists = [
+//   {
+//     index: 1,
+//     description: 'Wash the dishes',
+//     completed: false,
+//   },
+//   {
+//     index: 2,
+//     description: 'Complete To Do List project',
+//     completed: false,
+//   },
+// ];
 
-export default tasksLists;
+// const tasksLists = [];
+
+// const todoTasks = () => {
+//   if (localStorage.getItem("duties") === null) {
+//     tasksLists = [];
+//   } else {
+//     tasksLists = JSON.parse(localStorage.getItem("duties"));
+//   }
+// };
+
+// export default tasksLists;
