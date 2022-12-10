b215594b4c66f7adacbcc521f2dd978f6a34dae0
elarhadu <elarhadu@gmail.com>
2022-12-10 12:55:18 +0000

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import "lodash";
-import "./style.css";
+import 'lodash';
+import './style.css';
 
-const inputField = document.querySelector(".add-form input");
+const inputField = document.querySelector('.add-form input');
 let taskLists = [];
-const todoLists = document.querySelector(".tasks-list");
-const clearAll = document.querySelector(".complete");
+const todoLists = document.querySelector('.tasks-list');
 
 const tasksTodo = (todo) => {
-  const displayTasks = document.createElement("div");
-  displayTasks.innerHTML=''
+  const displayTasks = document.createElement('div');
+  displayTasks.innerHTML = '';
   displayTasks.innerHTML += `
      <div class = "todo-list" id = ${todo.index}>
      <div class = "checkbox-description-container">
@@ -29,62 +28,60 @@ const tasksTodo = (todo) => {
   todoLists.appendChild(displayTasks);
 };
 
-inputField.addEventListener("keypress", (e) => {
-  let todoLists = inputField.value;
-  if (e.key === "Enter" && todoLists) {
-    if (localStorage.getItem("duties") === null) {
+inputField.addEventListener('keypress', (e) => {
+  const todoLists = inputField.value;
+  if (e.key === 'Enter' && todoLists) {
+    if (localStorage.getItem('duties') === null) {
       taskLists = [];
     } else {
-      taskLists = JSON.parse(localStorage.getItem("duties"));
+      taskLists = JSON.parse(localStorage.getItem('duties'));
     }
-    let assignment = {
+    const assignment = {
       description: todoLists,
       completed: false,
       index: taskLists.length + 1,
     };
-    e.target.value = "";
+    e.target.value = '';
     taskLists.push(assignment);
-    localStorage.setItem("duties", JSON.stringify(taskLists));
+    localStorage.setItem('duties', JSON.stringify(taskLists));
     tasksTodo(assignment);
   }
 });
 
 window.removeTask = (index) => {
   const taskId = document.getElementById(index);
-  const deleteTask = JSON.parse(localStorage.getItem("duties"));
-  const newTodo = deleteTask.filter((task) => {
-    return task.index !== index;
-  });
+  const deleteTask = JSON.parse(localStorage.getItem('duties'));
+  const newTodo = deleteTask.filter((task) => task.index !== index);
   newTodo.forEach((task, i) => {
-    task.index=i+1
+    task.index = i + 1;
   });
-  localStorage.setItem("duties", JSON.stringify(newTodo));
+  localStorage.setItem('duties', JSON.stringify(newTodo));
   taskId.remove();
 };
 
-const tasks = JSON.parse(localStorage.getItem("duties")) || [];
-if (localStorage.getItem("duties")) {
+const tasks = JSON.parse(localStorage.getItem('duties')) || [];
+if (localStorage.getItem('duties')) {
   tasks.forEach((task) => {
     tasksTodo(task);
   });
 }
 
-const editTasks = document.querySelectorAll(".user-task");
+const editTasks = document.querySelectorAll('.user-task');
 editTasks.forEach((task, index) => {
-  task.addEventListener("change", (e) => {
+  task.addEventListener('change', (e) => {
     const updatedTask = e.target.value;
-    taskLists = JSON.parse(localStorage.getItem("duties"));
+    taskLists = JSON.parse(localStorage.getItem('duties'));
     taskLists[index].description = updatedTask;
-    localStorage.setItem("duties", JSON.stringify(taskLists));
+    localStorage.setItem('duties', JSON.stringify(taskLists));
   });
 });
 editTasks.forEach((task, index) => {
-  task.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
+  task.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
       const updatedTask = e.target.value;
-      taskLists = JSON.parse(localStorage.getItem("duties"));
+      taskLists = JSON.parse(localStorage.getItem('duties'));
       taskLists[index].description = updatedTask;
-      localStorage.setItem("duties", JSON.stringify(taskLists));
+      localStorage.setItem('duties', JSON.stringify(taskLists));
     }
   });
 });
